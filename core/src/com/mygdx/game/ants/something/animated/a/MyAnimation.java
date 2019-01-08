4f90d61f5fff371cf08d4de1233abed18ce7f349
Levon Ayvazyan <levayv43@gmail.com>
2019-01-08 07:01:18 +0400

    MyAnimation class integrated with AnimatedEventSomething

--- core/src/com/mygdx/game/ants/something/animated/a/MyAnimation.java
+++ core/src/com/mygdx/game/ants/something/animated/a/MyAnimation.java
@@ -1,4 +1,51 @@
 package com.mygdx.game.ants.something.animated.a;
 
-class MyAnimation {
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class MyAnimation {
+    private Animation<TextureRegion> coreAnimation;
+    private TextureRegion frame;
+    private float coreFrameDur;
+    private int coreFrames;
+    private boolean loopingEndless = true;
+    private boolean startAnimCycle = false;
+    private float animationTime = 0f;
+
+    public MyAnimation(boolean loopingEndless){
+        this.loopingEndless = loopingEndless;
+    }
+    public void setCoreAnimation(Animation<TextureRegion> coreAnimation){
+        this.coreAnimation = coreAnimation;
+        coreFrames = coreAnimation.getKeyFrames().length;
+        coreFrameDur = (0.025f / coreFrames)*20; // 3fps ?
+    }
+    public TextureRegion updateFrame(float delta){ // inside Actor's Act method
+        if (startAnimCycle){
+            if (coreFrameDur * coreFrames < animationTime){ // todo checking if not looping
+                startAnimCycle = false;
+//                System.out.println("stop" + animationTime);
+            }else {
+//                System.out.println("start" + animationTime);
+                frame = coreAnimation.getKeyFrame(animationTime,true);
+                animationTime += delta;
+            }
+        }else {
+            if (loopingEndless){
+                frame = coreAnimation.getKeyFrame(animationTime,loopingEndless);
+                animationTime += delta;
+//                System.out.println(this.getName() + " at="+ animationTime);
+//                System.out.println(this.getName() + " FD="+ coreFrameDur*coreFrames*10);
+            }
+        }
+        frame = coreAnimation.getKeyFrame(animationTime,loopingEndless);
+        return frame;
+    }
+    public void startAnim(){
+        this.startAnimCycle = true;
+    }
+    public boolean drawOrNot(){
+        return loopingEndless || startAnimCycle;
+    }
+
 }
--- core/src/com/mygdx/game/ants/something/animated/event/a/AnimatedEventSomething.java
+++ core/src/com/mygdx/game/ants/something/animated/event/a/AnimatedEventSomething.java
@@ -5,20 +5,21 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.mygdx.game.WorldResAnimManager;
 import com.mygdx.game.ants.something.a.Something;
+import com.mygdx.game.ants.something.animated.a.MyAnimation;
 import com.mygdx.game.enums.EntityTex;
 import com.mygdx.game.enums.EntityAnimation;
 
 public class AnimatedEventSomething extends Something {
     int stateCount;
-//    MyAnimation animation = new MyAnimation();
+    private MyAnimation animation;
     // old
-    public boolean loopingEndless = true;
-    public boolean startAnimCycle = false;
-    public float animationTime = 0f;
+//    public boolean loopingEndless = true;
+//    public boolean startAnimCycle = false;
+//    public float animationTime = 0f;
 
-    private float coreFrameDur;
-    private int coreFrames;
-    protected Animation<TextureRegion> coreAnimation;
+//    private float coreFrameDur;
+//    private int coreFrames;
+//    protected Animation<TextureRegion> coreAnimation;
 
     public int tempINT;
     public String tempString;
@@ -29,6 +30,7 @@ public class AnimatedEventSomething extends Something {
                                   EntityAnimation entityAnim,
                                   int stateCount) {
         super(entityTex);
+        animation = new MyAnimation(true);
 //        super(entityTex, entityAnim);
         this.entityAnim = entityAnim;
         this.stateCount = stateCount;
@@ -40,43 +42,23 @@ public class AnimatedEventSomething extends Something {
 //    }
 
     public void setAnim(WorldResAnimManager animManager){
-        coreAnimation = animManager.getAnimationByID(this.entityAnim);
-        coreFrames = coreAnimation.getKeyFrames().length;
-        coreFrameDur = (0.025f / coreFrames)*20; // 3fps ?
+        animation.setCoreAnimation(animManager.getAnimationByID(this.entityAnim));
+
 //        coreFrameDur = 0.015f; // 60 fps !
-    }
-    public void setAnimEvent(){
 
     }
+
     @Override
     public void act(float delta) {
 //        if (loopingEndless || startAnimCycle){
 //            setVisible(false);
 //        }else
 //            setVisible(true);
-        if (startAnimCycle){
-            if (coreFrameDur * coreFrames *1 < animationTime){
-                startAnimCycle = false;
-                System.out.println("stop" + animationTime);
-            }else {
-                System.out.println("start" + animationTime);
-                texReg = coreAnimation.getKeyFrame(animationTime,true);
-                animationTime += delta;
-                System.out.println(this.getName() + " at="+ animationTime);
-                System.out.println(this.getName() + " FD="+ coreFrameDur * coreFrames *1);
-            }
-        }else {
-            if (loopingEndless){
-                texReg = coreAnimation.getKeyFrame(animationTime,loopingEndless);
-                animationTime += delta;
-//                System.out.println(this.getName() + " at="+ animationTime);
-//                System.out.println(this.getName() + " FD="+ coreFrameDur*coreFrames*10);
-            }
-        }
+        texReg = animation.updateFrame(delta);
     }
     @Override
     public void draw (Batch batch, float parentAlpha) {
-        if (loopingEndless || startAnimCycle){
+        if (animation.drawOrNot()){
             batch.draw(texReg, getX(), getY());
         }
     }
